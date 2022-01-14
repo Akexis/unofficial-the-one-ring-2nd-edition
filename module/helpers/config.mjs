96c7f342a7c7972d29725b94480938f78500d213
Alexis Abbott <Akexis@users.noreply.github.com>
2022-01-13 23:22:17 -0600

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const unofficial-the-one-ring-2nd-edition = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ unofficial-the-one-ring-2nd-edition.abilities = {
+  "str": "unofficial-the-one-ring-2nd-edition.AbilityStr",
+  "dex": "unofficial-the-one-ring-2nd-edition.AbilityDex",
+  "con": "unofficial-the-one-ring-2nd-edition.AbilityCon",
+  "int": "unofficial-the-one-ring-2nd-edition.AbilityInt",
+  "wis": "unofficial-the-one-ring-2nd-edition.AbilityWis",
+  "cha": "unofficial-the-one-ring-2nd-edition.AbilityCha"
+};
+
+unofficial-the-one-ring-2nd-edition.abilityAbbreviations = {
+  "str": "unofficial-the-one-ring-2nd-edition.AbilityStrAbbr",
+  "dex": "unofficial-the-one-ring-2nd-edition.AbilityDexAbbr",
+  "con": "unofficial-the-one-ring-2nd-edition.AbilityConAbbr",
+  "int": "unofficial-the-one-ring-2nd-edition.AbilityIntAbbr",
+  "wis": "unofficial-the-one-ring-2nd-edition.AbilityWisAbbr",
+  "cha": "unofficial-the-one-ring-2nd-edition.AbilityChaAbbr"
+};
